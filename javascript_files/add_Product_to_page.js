056f04474006c0a45fe7e5edb4fa7490c3a87601
Mohit <mohit9599411352@gmail.com>
2024-12-08 15:33:03 +0530

    version 1

---
+++ javascript_files/add_Product_to_page.js
@@ -0,0 +1,92 @@
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
+
+// adding products to cart through javascript
+function AddingProductsToPage() {
+  let allProducts = "";
+
+  products.forEach((product) => {
+    const item_html = `
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${(product.priceCents / 100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart-btn" data-product-id="${
+            product.id
+          }">
+            Add to Cart
+          </button>
+        </div>
+    `;
+
+    allProducts += item_html;
+  });
+
+  const productGrid = document.querySelector(".products-grid");
+  productGrid.innerHTML = allProducts;
+}
+AddingProductsToPage();
+
+// adding products to cart
+
+function adding_products_to_cart(cart) {
+  const addToCartButton = document.querySelectorAll(".js-add-to-cart-btn");
+
+  addToCartButton.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const productId = btn.dataset.productId;
+
+      const product = products.find((product) => product.id == productId);
+      cart.push({
+        id: product.id,
+        quantity: 1,
+      });
+      console.log(cart);
+    });
+  });
+}
+
+adding_products_to_cart(cart , products);
+
+
+
